cd870164f50f8f610be8f44c0b36ec808afff5e0
DrTheDraGNaK <jean-michel.giverne@edu.devinci.fr>
2023-10-26 10:41:00 +0200

    [save]

--- TP_AI_Project/Assets/Teams/BattleStar/Helpers/DetectMine.cs
+++ TP_AI_Project/Assets/Teams/BattleStar/Helpers/DetectMine.cs
@@ -8,7 +8,7 @@ namespace BattleStar
 {
     public static class DetectMine 
     {
-        public static bool MineDetected(GameData gameData, int spaceShipOwner, int spaceShipOwnerEnemy)
+        public static bool MineDetected(GameData gameData, int spaceShipOwner)
         {
             Vector2 _cirlcePos = gameData.SpaceShips[spaceShipOwner].Position + new Vector2(1f, 0);
             var hitColliders = Physics2D.OverlapCircle(_cirlcePos, 1f,  13);
